0d50490cf1c9f14a79eac0452a51889185968158
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-03-09 19:45:09 +0000

    Forms & Events

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -1,48 +1,25 @@
 import './App.css';
-import Info from './Info.js';
 import { PropTypes } from "prop-types";
 import { useState } from "react";
+import SearchBar from "./SearchBar";
 
 function App() {
+  const [data, setData] = useState({});
+
+  const updateData = (searchParams) => {
+    setData(searchParams);
+  };
+
   return (
     <div className="App">
-      <p>Hello World</p>
-      <Info title="Inventory" />
-      <ButtonsState />
+      <SearchBar callback={updateData} />
+      <p>Name: {"name" in data ? data["name"] : "No data to display"}</p>
+      <p>Price: {"name" in data ? data["price"] : "No data to display"}</p>
+      <p>Name: {"type" in data ? data["type"] : "No data to display"}</p>
+      <p>Name: {"brand" in data ? data["brand"] : "No data to display"}</p>
     </div>
   );
 }
 
-
-function ButtonsState() {
-  const [title, setTitle] = useState("");
-  const [count, setCount] = useState(0)
-
-  const updateTitleClicked = () => {
-    setTitle("We now have a title!");
-  }
-
-  const updateCounterClicked = () => {
-    setCount(count + 1);
-  }
-
-  return (
-    <div>
-      <Data title={title} count={count} />
-      <button onClick={updateTitleClicked}>Update Title</button>
-      <button onClick={updateCounterClicked}>Update Counter</button>
-    </div>
-  );
-}
-
-
-function Data(props) {
-  return (
-    <div>
-      <p>Title: {props.title}</p>
-      <p>Count: {props.count}</p>
-    </div>
-  )
-}
 
 export default App;
